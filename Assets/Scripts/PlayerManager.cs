7aead51c2083591ff61c2f88da55f42d93bb3e43
Rodrigue Maurouard <r.maurouard@rubika-edu.com>
2016-05-06 12:13:17 +0200

    Turn manager

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,149 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using DG.Tweening;
+//public class PlayerManager : MonoBehaviour {
+//    public Player[] players;
+//    public Player selectedPlayer;
+//    public Player advancedPlayer;
+
+//    public GameObject[] indicatorPlayer;
+//    public Vector3 initialCamPosition ;
+//    bool isInCombo;
+//    // Use this for initialization
+//    void Start () {
+//	    for(int i = 0; i < players.Length; i++)
+//        {
+//            players[i].playerID = i;
+//            indicatorPlayer[i].SetActive(false);
+//        }
+//        isInCombo = false;
+//        initialCamPosition = Camera.main.transform.position;
+//    }
+	
+//	// Update is called once per frame
+//	void Update () {
+//        Debug.Log(isInCombo);
+//        if (Input.GetKey(KeyCode.G))
+//        {
+//            //Camera.main.DOFieldOfView(30, 1);
+            
+            
+//        }
+
+//        if (Input.GetKey(KeyCode.H))
+//        {
+            
+//        }
+//        float h = Input.GetAxisRaw("L_YAxis_1");
+//        float v = Input.GetAxisRaw("L_XAxis_1");
+
+//        float triggerRight = Input.GetAxisRaw("TriggersR_1");
+//        float triggerLeft = Input.GetAxisRaw("TriggersL_1");
+
+//        if (TurnManager.GetInstance().whoPlays == 0)
+//        {
+//            if (Input.GetKey(KeyCode.A) || v > 0.4 && advancedPlayer == null )
+//            {
+//                if (!players[0].isAdvanced)
+//                {
+//                    selectedPlayer = players[0];
+//                    HideIndicators();
+//                    indicatorPlayer[0].SetActive(true);
+//                }
+//            }
+//            if (Input.GetKey(KeyCode.Z) || h > 0.4 && advancedPlayer == null)
+//            {
+//                if (!players[1].isAdvanced)
+//                {
+//                    selectedPlayer = players[1];
+//                    HideIndicators();
+//                    indicatorPlayer[1].SetActive(true);
+//                }
+//            }
+//            if (Input.GetKey(KeyCode.E) || h < -0.4 && advancedPlayer == null)
+//            {
+//                if (!players[2].isAdvanced)
+//                {
+//                    selectedPlayer = players[2];
+//                    HideIndicators();
+//                    indicatorPlayer[2].SetActive(true);
+//                }
+//            }
+
+//            if( h == 0 && v == 0)
+//            {
+//                selectedPlayer = null;
+//                HideIndicators();
+//            }
+
+//            if (triggerRight > 0.5f && selectedPlayer != null && !selectedPlayer.isAdvanced )
+//            {
+//                allBack();
+//                selectedPlayer.isAdvanced = true;
+//                advancedPlayer = selectedPlayer;
+//                advancedPlayer.Advance();
+                
+//            }
+//            if (triggerLeft > 0.5f && advancedPlayer != null)
+//            {
+//                advancedPlayer.isAdvanced = false;
+//                advancedPlayer.Back();
+//                advancedPlayer = null;
+//                Camera.main.transform.DOLookAt(initialCamPosition, 0.5f);
+//                Camera.main.DOFieldOfView(60, 1f);
+//            }
+
+//            if (Input.GetButtonDown("A_button_1") && advancedPlayer.isAdvanced && advancedPlayer.canAttack && !isInCombo)
+//            {
+//                advancedPlayer.AttackA();
+//            }
+
+//            if (Input.GetButtonDown("B_button_1") && advancedPlayer.isAdvanced && advancedPlayer.canAttack && !isInCombo)
+//            {
+//                advancedPlayer.AttackB();
+//            }
+
+//            if(Input.GetButtonDown("X_button_1") && advancedPlayer.isAdvanced && advancedPlayer.canAttack && !isInCombo)
+//            {
+//                StartCoroutine(MyCombo());
+//            }
+
+//            if (Input.GetButtonDown("A_button_1") && advancedPlayer.isAdvanced && advancedPlayer.canAttack && isInCombo)
+//            {
+//                advancedPlayer.AttackCombo();
+//            }
+//        }
+//    }
+
+//    public void allBack()
+//    {
+//        for (int i = 0; i < players.Length; i++)
+//        {
+//            players[i].Back();
+//        }
+//            Camera.main.transform.DOLookAt(initialCamPosition, 0.5f);
+//            Camera.main.DOFieldOfView(60, 1f);
+        
+//    }
+
+//    void HideIndicators()
+//    {
+//        for(int i =0; i < indicatorPlayer.Length; i++)
+//        {
+//            indicatorPlayer[i].SetActive(false);
+//        }
+//    }
+
+//    IEnumerator MyCombo()
+//    {
+//        isInCombo = true;
+//        float timer = 1;
+//        while (timer > 0)
+//        {
+//            timer -= Time.deltaTime;
+//            yield return new WaitForEndOfFrame();
+//        }
+//        isInCombo = false;
+//        yield break;
+//    }
+//}
---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+public class TurnManager : MonoBehaviour {
+
+    private static TurnManager instance = null;
+
+    public Image _timer;
+    public float _maxTimeTurn;
+    public float _currentTimeTurn;
+    public bool _isPlaying;
+
+    public int _whoPlays;
+
+    public Text _textTurn;
+    //public PlayerManager _allPlayers;
+
+    public static TurnManager GetInstance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        _maxTimeTurn = 10;
+        _currentTimeTurn = _maxTimeTurn;
+        _isPlaying = false;
+        _whoPlays = 0;
+        _textTurn.text = "Player Turn";
+    }
+
+    void Update()
+    {
+        _currentTimeTurn -= Time.deltaTime;
+        if (!_isPlaying)
+        {
+            _isPlaying = true;
+            StartCoroutine(WaitForCdfloat(_maxTimeTurn));
+        }
+        if(_currentTimeTurn <= 0)
+        {
+            changeTurn();
+        }
+    }
+
+    void changeTurn()
+    {
+        _currentTimeTurn = _maxTimeTurn;
+        _timer.fillAmount = 1;
+        StartCoroutine(WaitForCdfloat(_maxTimeTurn));
+        if(_whoPlays == 0)
+        {
+            _whoPlays = 1;
+            _textTurn.text = "Monster Turn";
+            //_allPlayers.allBack();
+            //_allPlayers.advancedPlayer = null;
+        }
+        else if (_whoPlays == 1)
+        {
+            _whoPlays = 0;
+            _textTurn.text = "Player Turn";
+        }
+        _textTurn.transform.DOShakePosition(0.1f, 10);
+    }
+
+    public IEnumerator WaitForCdfloat(float parCdTimer)
+    {
+            _timer.fillAmount = 1;
+            yield return StartCoroutine(fillIcon(_timer, parCdTimer));
+    }
+
+    public IEnumerator fillIcon(Image parIcon, float parCdTimer)
+    {
+        float _timer = _maxTimeTurn;
+        while (_timer <= parCdTimer)
+        {
+            parIcon.fillAmount = _timer / parCdTimer;
+            _timer -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        _timer = 0;
+    }
+}
