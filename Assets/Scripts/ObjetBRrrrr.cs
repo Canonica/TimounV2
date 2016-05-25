c929eaec5816e346420245b5df2c2956556b4e6c
Rodrigue Maurouard <r.maurouard@rubika-edu.com>
2016-05-25 18:34:01 +0200

    objet brrrrrr

---
+++ Assets/Scripts/ObjetBRrrrr.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using XInputDotNetPure;
+public class ObjetBRrrrr : MonoBehaviour {
+
+    bool isEnter;
+    public CanvasGroup canvasGroup;
+    bool isButtonA;
+	// Use this for initialization
+	void Start () {
+        canvasGroup.DOFade(0, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.DOShakeRotation(0.1f, 1f, 5).OnComplete(() => this.transform.DOKill());
+        if (XInput.instance.getButton(0, 'A') == ButtonState.Pressed && isEnter && !isButtonA)
+        {
+            isButtonA = true;
+            canvasGroup.DOFade(0, 0.3f);
+            Destroy(this.gameObject);
+
+        }else if (XInput.instance.getButton(0, 'A') == ButtonState.Released && isButtonA)
+        {
+            isButtonA = false;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            canvasGroup.DOFade(1, 0.3f);
+            isEnter = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canvasGroup.DOFade(0, 0.3f);
+            isEnter = false;
+        }
+    }
+}
